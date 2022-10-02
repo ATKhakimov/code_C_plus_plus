3a36d889f3d5271598aba97f5cd000932e96af32
ATKhakimov <nodosty@gmail.com>
2022-10-02 19:28:59 +0300

    sizeof(struct) = 6

--- week4/Task 4.3 (date_time).cpp
+++ week4/Task 4.3 (date_time).cpp
@@ -1,35 +1,33 @@
 #include <iostream>
 
 
-using std::cout;
-using std::endl;
-//using std::string;
-using std::cin;
-
-
+#pragma pack(push, 1)
 struct Date_time 
 {
-	unsigned short day : 5;   
-	unsigned short month : 4; 
-	unsigned short year : 7;  
-	unsigned short hour : 5;  
-	unsigned short minute : 6;
-	unsigned short second : 6;
-	unsigned short millisecond : 10;
+	unsigned millisecond : 10;
+	unsigned year : 7; 
+	unsigned day : 5;   
+	unsigned month : 4; 
+	unsigned hour : 5;  
+	unsigned minute : 6;
+	unsigned second : 6;
+	
 };
+#pragma pack(pop)
 
 int main()
 {	
 	Date_time date; 
-	int a;      
-	cout << "Input Day (1-31):" << '\t'; cin >> a; date.day = a; /* we cannt use "cin >> d.day", because there are no pointers to bit-fields*/
-	cout << "Input Month (1-12):" << '\t'; cin >> a; date.month = a;
-	cout << "Input Year (00-99) :" << '\t'; cin >> a; date.year = a;
-	cout << endl << "Input Hour (0-24):" << '\t'; cin >> a; date.hour = a;
-	cout << "Input Minute (0-60):" << '\t'; cin >> a; date.minute = a;
-	cout << "Input Seconds (0-60):" << '\t'; cin >> a; date.second = a;
-	cout << "Input Milliseconds (0-1000):" << '\t'; cin >> a; date.millisecond = a;
+	int a;
+	std::cout << sizeof(Date_time) << std::endl;      
+	std::cout << "Input Day (1-31):" << '\t'; std::cin >> a; date.day = a; 
+	std::cout << "Input Month (1-12):" << '\t'; std::cin >> a; date.month = a;
+	std::cout << "Input Year (00-99) :" << '\t'; std::cin >> a; date.year = a;
+	std::cout << std::endl << "Input Hour (0-24):" << '\t'; std::cin >> a; date.hour = a;
+	std::cout << "Input Minute (0-60):" << '\t'; std::cin >> a; date.minute = a;
+	std::cout << "Input Seconds (0-60):" << '\t'; std::cin >> a; date.second = a;
+	std::cout << "Input Milliseconds (0-1000):" << '\t'; std::cin >> a; date.millisecond = a;
 
-	cout << endl << "********** Date info **********" << endl << date.day << "." << date.month << ".20" << date.year << " " << endl;
-	cout << date.hour << ":" << date.minute << ":" << date.second << ":" << date.millisecond << endl;
+	std::cout << std::endl << "********** Date info **********" << std::endl << date.day << "." << date.month << ".20" << date.year << " " << std::endl;
+	std::cout << date.hour << ":" << date.minute << ":" << date.second << ":" << date.millisecond << std::endl;
 }
