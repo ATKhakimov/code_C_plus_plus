73b83b95a426a182cdbdf95e4cbce6e523d221ab
ATKhakimov <nodosty@gmail.com>
2022-10-04 14:07:15 +0300

    update 2.0

--- week4/task4.4.cpp
+++ week4/task4.4.cpp
@@ -24,11 +24,17 @@ int main()
     std::cout << "Write the number of the month from 1 to 12:" << '\t'; std::cin>>a;
 	std::cout << "Enter year in 4-number format:" << '\t'; std::cin >> b;
 	switch(static_cast<int>(a)){
-	case static_cast<int>(Month::February):
+	case 2:
 		if( b % 4 == 0){ answer = 29;}
 		else{ answer = 28;}
 		break;
-	case static_cast<int>(Month::January) | static_cast<int>(Month::March) | static_cast<int>(Month::May) | static_cast<int>(Month::July) | static_cast<int>(Month::August) | static_cast<int>(Month::October) | static_cast<int>(Month::December)  :
+	case 1:
+	case 3:
+	case 5:
+	case 7:
+	case 8:
+	case 10:
+	case 12:
 		answer = 31;
 		break;
 	default:
